da9a80d9f254792bb9f4e617194421b02954cd3f
gurelbs <guri240@gmail.com>
2021-10-10 05:54:37 +0300

    updated

--- index.js
+++ index.js
@@ -20,5 +20,43 @@ if (prod) {
 		res.sendFile(file, { root: __dirname })
 	})
 }
+process.on('beforeExit', code => {
+  // Can make asynchronous calls
+  setTimeout(() => {
+    console.log(`Process will exit with code: ${code}`)
+    process.exit(code)
+  }, 100)
+})
+
+process.on('exit', code => {
+  // Only synchronous calls
+  console.log(`Process exited with code: ${code}`)
+})
+process.on('SIGTERM', signal => {
+  console.log(`Process ${process.pid} received a SIGTERM signal`)
+  process.exit(0)
+})
+
+process.on('SIGINT', signal => {
+  console.log(`Process ${process.pid} has been interrupted`)
+  process.exit(0)
+})
+process.on('uncaughtException', err => {
+  console.log(`Uncaught Exception: ${err.message}`)
+  process.exit(1)
+})
+process.on('unhandledRejection', (reason, promise) => {
+  console.log('Unhandled rejection at ', promise, `reason: ${err.message}`)
+  process.exit(1)
+})
+process.on('ENOTCONN', _ => {
+  server.close(() => {
+    process.exit(0)
+  })
+  // If server hasn't finished in 1000ms, shut down process
+  setTimeout(() => {
+    process.exit(0)
+  }, 1000).unref() // Prevents the timeout from registering on event loop
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
